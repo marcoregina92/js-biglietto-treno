b25e5a61aab44dc2fb63ad55de177fbb4d361ac6
Marco <marcoregina92@gmail.com>
2023-10-26 19:25:18 +0200

    completa esercizio

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-const kilometers = prompt("Quanti km vorresti percorrere?")
+const km = prompt("Quanti km vorresti percorrere?")
+const age = prompt("Quanti anni hai?")
+const rate = 0.21
+
+let finalPrice = km * 0.21;
+console.log(finalPrice)
+
+if ( !isFinite(km) || !isFinite(age)) {
+    document.getElementById("titolo").innerHTML = `Ops, qualcosa è andato storto.`;
+}
+else if (age < 18) {
+    finalPrice = (finalPrice * 0.8).toFixed(2)
+    document.getElementById("titolo").innerHTML = `Costo totale del biglietto scontato del 20%:`;
+    document.getElementById("prezzoFinale").innerHTML = `${finalPrice} €`;
+}
+else if (age >= 65) {
+    finalPrice = (finalPrice * 0.6).toFixed(2)
+    document.getElementById("titolo").innerHTML = `Costo totale del biglietto scontato del 40%:`;
+    document.getElementById("prezzoFinale").innerHTML = `${finalPrice} €`;
+}
+else {
+    document.getElementById("titolo").innerHTML = `Costo totale del biglietto:`;
+    document.getElementById("prezzoFinale").innerHTML = `${finalPrice} €`;
+}
+
